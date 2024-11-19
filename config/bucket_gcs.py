4ba099616eea41901de383657c4610ff96c69863
Lavibuu La <121961129+lavibula@users.noreply.github.com>
2024-11-19 22:32:06 +0700

    update

--- config/bucket_gcs.py
+++ config/bucket_gcs.py
@@ -1,6 +1,6 @@
 from google.cloud import storage
 
-client = storage.Client.from_service_account_json("./config/btcanalysishust-52ca6add0469.json")
+client = storage.Client.from_service_account_json("config/btcanalysishust-495a3a227f22.json")
 
 buckets = list(client.list_buckets())
 print("Buckets:")
